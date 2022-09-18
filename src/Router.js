9ec84bbce470a02c2203636f9baf7dbeb5945013
shine-jung <79754613+shine-jung@users.noreply.github.com>
2022-09-18 16:41:24 +0900

    feat: MUI 테마 적용, 기본 헤더 추가

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/common/Header";
 import Main from "./pages/Main";
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
