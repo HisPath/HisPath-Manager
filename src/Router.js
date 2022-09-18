2673c4aa9a14917d73a7cc795971d3aa2190f968
davidpiao <churry9797@gmail.com>
2022-09-19 02:49:50 +0900

    feat: MUI 테이블 추가

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
+import Student from "./pages/Student";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/studentList" element={<Student />} />
       </Routes>
     </BrowserRouter>
   );
