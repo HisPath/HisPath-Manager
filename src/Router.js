4528c167d4e86a4376f491f1804cc8624ac8cb6d
davidpiao <churry9797@gmail.com>
2022-09-21 01:00:13 +0900

    chore: 이름처리

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import Student from "./pages/Student";
+import Students from "./pages/Students";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/studentList" element={<Student />} />
+        <Route path="/studentList" element={<Students />} />
       </Routes>
     </BrowserRouter>
   );
