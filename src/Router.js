010ead7dcee1a8e86642f6894a300a1f8e3beae7
shine-jung <79754613+shine-jung@users.noreply.github.com>
2022-09-30 20:12:28 +0900

    feat: sample custom components

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/common/Header";
 import Main from "./pages/Main";
 import Mileage from "./pages/Mileage";
+import Sample from "./pages/Sample";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mileage" element={<Mileage />} />
+        <Route path="/sample" element={<Sample />} />
       </Routes>
     </BrowserRouter>
   );
