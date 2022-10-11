bd5ce86a72e06d15dc53051535575f6208a9f8fb
lukehongg <lukehongg@handong.ac.kr>
2022-10-12 03:44:34 +0900

    [#17]: 공지 완성

--- src/pages/Notice.js
+++ src/pages/Notice.js
@@ -1,28 +1,12 @@
-// import { Component, useState } from 'react';
-// import { Box, Button, Container, Modal, styled, Typography } from '@mui/material';
-// import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
-// import OpenInFullIcon from '@mui/icons-material/OpenInFull';
-// import EditIcon from '@mui/icons-material/Edit';
-// import DeleteIcon from '@mui/icons-material/DeleteOutline';
-// import { useRecoilState } from 'recoil';
 import Notice_TabTT from '../components/notice/Notice_TabTT';
 import Header from '../components/common/Header';
 import TT from '../components/notice/tables/TT';
-// const Header = styled('div')({
-//   height: '15%',
-//   display: 'flex',
-//   justifyContent: 'space-between',
-//   alignItems: 'flex-end',
-//   paddingBottom: 24,
-// });
 
 function Notice() {
   return (
     <div>
-      {/* <Notice_TabTT /> */}
       <TT />
     </div>
   );
 }
-
 export default Notice;
