0f4fe05e3e6f318d9d09d689a95537ed355f46a4
davidpiao <churry9797@gmail.com>
2022-11-02 23:05:16 +0900

    feat[#27] scholarship 페이지 탭

--- src/pages/ScholarshipManagement.js
+++ src/pages/ScholarshipManagement.js
@@ -8,14 +8,19 @@ import {
   styled,
   Typography,
 } from "@mui/material";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import { useRecoilState } from "recoil";
 import { scholarshipState } from "../atom";
+import * as React from "react";
 import axios from "axios";
 import ScholarshipRegisteredTable from "../components/Scholarship/ScholarshipRegisteredTable";
 import ViewScholarshipRegistered from "../components/Scholarship/ViewScholarshipRegistered";
 import { SelectColumnFilter } from "../components/Scholarship/filters";
-import LinkTabs from "../components/Scholarship/ScholarshipTabs";
+import ScholarshipListTable from "../components/Scholarship/ScholarshipListTable";
 import { InputLabel } from "@mui/material";
+import BasicTabs from "../components/Scholarship/ScholarshipTabs";
 
 const Header = styled("div")({
   display: "flex",
@@ -23,60 +28,6 @@ const Header = styled("div")({
   alignItems: "flex-end",
   paddingBottom: 24,
 });
-
-const Article = styled(Box)({
-  height: "calc(100vh - 200px)",
-});
-
-const columns = [
-  {
-    accessor: "departmentName",
-    Header: "학부",
-    Filter: SelectColumnFilter,
-  },
-  {
-    accessor: "major1Name",
-    Header: "1전공",
-    Filter: SelectColumnFilter,
-  },
-  {
-    accessor: "major2Name",
-    Header: "2전공",
-    Filter: SelectColumnFilter,
-  },
-  {
-    accessor: "name",
-    Header: "이름",
-  },
-  {
-    accessor: "studentNum",
-    Header: "학번",
-  },
-  {
-    accessor: "totalWeight",
-    Header: "가중치",
-  },
-  {
-    accessor: "phone",
-    Header: "전화번호",
-  },
-  {
-    accessor: "email",
-    Header: "이메일",
-  },
-];
-
-const modalStyle = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  width: 800,
-  p: 3.5,
-  borderRadius: 4,
-};
 
 function ScholarshipManagement() {
   const [init, setInit] = useState(false);
@@ -87,6 +38,13 @@ function ScholarshipManagement() {
     setCurrentId(id);
     setOpenView(true);
   };
+
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   const handleCloseView = () => setOpenView(false);
   const loadData = () => {
     axios
@@ -104,27 +62,63 @@ function ScholarshipManagement() {
       <Header>
         <Typography variant="h5">마일리지 장학금 관리 페이지</Typography>
       </Header>
-      <LinkTabs></LinkTabs>
-      <Article>
-        {init ? (
-          <ScholarshipRegisteredTable
-            columns={columns}
-            data={scholarships}
-            handleOpenView={handleOpenView}
-          />
-        ) : (
-          <Backdrop
-            sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
-            open={true}
-          >
-            <CircularProgress color="inherit" />
-          </Backdrop>
-        )}
-      </Article>
+      <BasicTabs></BasicTabs>
+      {/* <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="마일리지 장학금 신청자 목록" {...a11yProps(0)} />
+          <Tab label="마일리지 장학금 수혜자 명단" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <Article>
+          {init ? (
+            <ScholarshipRegisteredTable
+              columns={columns}
+              data={scholarships}
+              initialState={{ showColumnFilters: true }}
+              muiTableHeadCellFilterTextFieldProps={{
+                sx: { m: "0.5rem 0", width: "100%" },
+                variant: "outlined",
+              }}
+              handleOpenView={handleOpenView}
+            />
+          ) : (
+            <Backdrop
+              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={true}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>
+          )}
+        </Article>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Article>
+          {init ? (
+            <ScholarshipListTable
+              columns={columns}
+              data={scholarships}
+              handleOpenView={handleOpenView}
+            />
+          ) : (
+            <Backdrop
+              sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+              open={true}
+            >
+              <CircularProgress color="inherit" />
+            </Backdrop>
+          )}
+        </Article>
+      </TabPanel>
+
       <Modal open={openView} onClose={handleCloseView}>
         <Box sx={modalStyle}>
           <Typography variant="h6" component="h2">
-            (2022-2) 마일리지 신청 목록
+            (2022-2) 마일리지 목록
           </Typography>
           <InputLabel>[전산전자] 박성진 (21700266)</InputLabel>
           <ViewScholarshipRegistered
@@ -132,7 +126,7 @@ function ScholarshipManagement() {
             handleClose={handleCloseView}
           />
         </Box>
-      </Modal>
+      </Modal> */}
     </Container>
   );
 }
