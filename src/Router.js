5405075446522bc520736f86246ab01a7e70fb77
lukehongg <lukehongg@handong.ac.kr>
2022-11-07 23:59:05 +0900

    [#37]:공지수정

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,25 @@
-import { Box, Toolbar } from "@mui/material";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/common/Header";
-import Main from "./pages/Main";
-import MileageActivity from "./pages/MileageActivity";
-import MileageParticipant from "./pages/MileageParticipant";
-import Sample from "./pages/Sample";
-import Notice from "./pages/Notice";
-import Post from "./pages/Post";
-import AddPost from "./pages/AddPost";
-import EditPost from "./pages/EditPost";
-import AxiTest from "./AxiTest";
-import Student from "./pages/Student";
-import Department from "./pages/Department";
-import Major from "./pages/Major";
-import Category from "./pages/Category";
-import ScholarshipManagement from "./pages/ScholarshipManagement";
-import ScholarshipList from "./pages/ScholarshipList";
-import AdminManagement from "./pages/AdminManagement";
-import { drawerWidth } from "./constants/commons";
-import Sidebar from "./components/common/Sidebar";
+import { Box, Toolbar } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/common/Header';
+import Main from './pages/Main';
+import MileageActivity from './pages/MileageActivity';
+import MileageParticipant from './pages/MileageParticipant';
+import Sample from './pages/Sample';
+import Notice from './pages/Notice';
+import Post from './pages/Post';
+import AddPost from './pages/AddPost';
+import EditPost from './pages/EditPost';
+import AxiTest from './AxiTest';
+import Student from './pages/Student';
+import Department from './pages/Department';
+import Major from './pages/Major';
+import Category from './pages/Category';
+import ScholarshipManagement from './pages/ScholarshipManagement';
+import ScholarshipList from './pages/ScholarshipList';
+import AdminManagement from './pages/AdminManagement';
+import { drawerWidth } from './constants/commons';
+import Sidebar from './components/common/Sidebar';
+import { Login } from './pages/Login';
 
 function Router() {
   return (
@@ -41,10 +42,7 @@ function Router() {
             <Route path="/" element={<Main />} />
             <Route path="/axitest" element={<AxiTest />} />
             <Route path="/mileage/activity" element={<MileageActivity />} />
-            <Route
-              path="/mileage/participant"
-              element={<MileageParticipant />}
-            />
+            <Route path="/mileage/participant" element={<MileageParticipant />} />
             <Route path="/sample" element={<Sample />} />
             <Route path="/notice" element={<Notice />} />
             <Route path="/addpost" element={<AddPost />} />
@@ -55,12 +53,10 @@ function Router() {
             <Route path="/department" element={<Department />} />
             <Route path="/category" element={<Category />} />
 
-            <Route
-              path="/scholarship/management"
-              element={<ScholarshipManagement />}
-            />
+            <Route path="/scholarship/management" element={<ScholarshipManagement />} />
             <Route path="/scholarship/list" element={<ScholarshipList />} />
             <Route path="/admin/management" element={<AdminManagement />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </Box>
       </Box>
