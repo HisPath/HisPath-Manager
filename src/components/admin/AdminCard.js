886ca41e5da65a33e1a46437c4c71d00e0a8e124
davidpiao <63008958+davidpiao@users.noreply.github.com>
2022-11-25 21:25:29 +0900

    [#106] 학부 카드 이미지 추가 (#107)
    * feat: MUI 테이블 추가
    
    * feat: MUI 테이블 추가
    
    * feat: Student router 설정
    
    * chore: fixed page name
    
    * chore: fixed page name
    
    * chore: 이름처리
    
    * feat: 학생 CRUD
    
    * chore: fixed modal layout
    
    * feat: 학생 관리 페이지 API 연동
    
    * feat[#2] 마일리지 장학금 신청 페이지
    
    * feat[#2] 마일리지 장학금 신청자 관리 페이지
    
    * fix[#2] 마일리지 장학금 신청자 관리 페이지 수정
    
    * feat[#24] 관리자 권한 페이지 완성
    
    * feat[#24] 마일리지 신청자 페이지 틀 구현
    
    * hotfix: admin management name fix
    
    * hotfix: deleted 중복 import
    
    * feat[#27] 장학금 수혜 페이지 구현
    
    * feat[#27] scholarship API 연동
    
    * feat[#27] scholarship 페이지 탭
    
    * feat[#27] 장학금 페이지 분리
    
    * [feat#34] 카테고리, 학부, 전공 관리 페이지 구현
    
    * fix[#34] API 연동 문제 해결
    
    * feat[#34] 학부, 전공, 카테고리 필드 추가
    
    * [fix#34] 장학금 신청자 페이지 수정
    
    * fix: 장학금 수혜자 페이지
    
    * fix[#34] Scholarship API 하드코딩 수정
    
    * build file upload test
    
    * hotfix: 중복 call 수정
    
    * feat[#27] Student, Manager API 정리
    
    * feat[#27] 장학금 API 수정
    
    * hotfix: 학생 수정 API 문제 해결
    
    * [feat#27] 장학금 수혜자 차트 구현
    
    * [feat#27] 학부, 전공, 카테고리 CRUD API 구현
    
    * [fix#27] 전공 API 에러 수정
    
    * [fix#27] API 전체 경로 수정
    
    * [fix#27] 관리자 권한 API 경로 수정
    
    * [fix#27] 학부, 카테고리 CRUD API 수정
    
    * hotfix: student page loading error fix
    
    * hotfix: deleted unused tab
    
    * [feat#27] 마일리지 수혜자 차트 더미데이터 추가
    
    * [fix#27] 테이블 사이즈 및 위치 수정
    
    * [fix#27] 공지 컨테이너 페이퍼 적용
    
    * [feat#27]마일리지 수혜자  차트 API 연결
    
    * hotfix: router 충돌 해결
    
    * hotfix: 마일리지 차트 데이터 수정
    
    * hotfix: 마일리지 차트 데이터 수정
    
    * [fix#27] 마일리지 차트 API 연동 완료
    
    * hotfix: set default semester
    
    * [fix#27] 로그 기록 삭제
    
    * fix: 학생 이미지 하드코딩 삭제
    
    * [feat#106] 학부 카드 이미지 추가

--- src/components/admin/AdminCard.js
+++ src/components/admin/AdminCard.js
@@ -1,18 +1,23 @@
-import { Box, Chip, Card, Divider, Typography } from '@mui/material';
-import UserImg from '../../assets/user.png';
-import Image from '../image';
+import { Box, Chip, Card, Divider, Typography } from "@mui/material";
+import UserImg from "../../assets/user.png";
+import Image from "../image";
 
-function AdminCard({ manager, setCurrentId, handleClickOpenSuper, handleClickOpenNormal }) {
+function AdminCard({
+  manager,
+  setCurrentId,
+  handleClickOpenSuper,
+  handleClickOpenNormal,
+}) {
   const Chips = ({ id, power }) => {
     return (
-      <Box sx={{ display: 'flex', gap: 2, justifyContent: 'center', pl: 1.3 }}>
+      <Box sx={{ display: "flex", gap: 2, justifyContent: "center", pl: 1.3 }}>
         <Chip
           color="success"
           onClick={() => {
             setCurrentId(+id);
             handleClickOpenSuper();
           }}
-          variant={power === 2 ? 'filled' : 'outlined'}
+          variant={power === 2 ? "filled" : "outlined"}
           size="lg"
           label="슈퍼"
         />
@@ -23,35 +28,46 @@ function AdminCard({ manager, setCurrentId, handleClickOpenSuper, handleClickOpe
             handleClickOpenNormal();
           }}
           size="lg"
-          variant={power === 1 ? 'filled' : 'outlined'}
+          variant={power === 1 ? "filled" : "outlined"}
           label="일반"
         />
         <Chip
           color="error"
           onClick={function () {}}
           size="lg"
-          variant={power === 0 ? 'filled' : 'outlined'}
+          variant={power === 0 ? "filled" : "outlined"}
           label="미승인"
         />
       </Box>
     );
   };
   return (
-    <Card sx={{ textAlign: 'center' }}>
-      <Box sx={{ position: 'relative' }}>
-        <Image src={UserImg} ratio="16/9" />
+    <Card sx={{ textAlign: "center" }}>
+      <Box sx={{ position: "relative" }}>
+        <Image src={`${manager.profile}`} ratio="16/9" />
       </Box>
-      <Box display="grid" gridTemplateColumns="repeat(1, 1fr)" sx={{ py: 1, mb: 4.5 }}>
-        <Typography variant="subtitle1" sx={{ mt: 6, mb: 0.5, color: 'black' }}>
+      <Box
+        display="grid"
+        gridTemplateColumns="repeat(1, 1fr)"
+        sx={{ py: 1, mb: 4.5 }}
+      >
+        <Typography variant="subtitle1" sx={{ mt: 6, mb: 0.5, color: "black" }}>
           {manager.department} | {manager.name}
         </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary', fontWeight: 'bolder' }}>
+        <Typography
+          variant="body2"
+          sx={{ color: "text.secondary", fontWeight: "bolder" }}
+        >
           {manager.email}
         </Typography>
       </Box>
-      <Divider sx={{ borderStyle: 'dashed' }} />
+      <Divider sx={{ borderStyle: "dashed" }} />
       <Box display="grid" gridTemplateColumns="repeat(1, 1fr)" sx={{ py: 2 }}>
-        <Typography variant="caption" component="div" sx={{ mb: 0.75, color: 'text.disabled' }}>
+        <Typography
+          variant="caption"
+          component="div"
+          sx={{ mb: 0.75, color: "text.disabled" }}
+        >
           권한
         </Typography>
         <Chips id={manager.id} power={manager.power} />
