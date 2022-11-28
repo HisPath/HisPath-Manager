8dd068694c1f0ad46b6bd295fed86432047957b9
davidpiao <63008958+davidpiao@users.noreply.github.com>
2022-11-28 17:08:17 +0900

    [#106] 학부, 전공 칼럼 제거 (#113)
    * feat: MUI 테이블 추가
    
    * feat: MUI 테이블 추가
    
    * feat: Student router 설정
    
    * chore: fixed page name
    
    * chore: fixed page name
    
    * chore: 이름처리
    
    * feat: 학생 CRUD
    
    * chore: fixed modal layout
    
    * feat: 학생 관리 페이지 API 연동
    
    * feat[#2] 마일리지 장학금 신청 페이지
    
    * feat[#2] 마일리지 장학금 신청자 관리 페이지
    
    * fix[#2] 마일리지 장학금 신청자 관리 페이지 수정
    
    * feat[#24] 관리자 권한 페이지 완성
    
    * feat[#24] 마일리지 신청자 페이지 틀 구현
    
    * hotfix: admin management name fix
    
    * hotfix: deleted 중복 import
    
    * feat[#27] 장학금 수혜 페이지 구현
    
    * feat[#27] scholarship API 연동
    
    * feat[#27] scholarship 페이지 탭
    
    * feat[#27] 장학금 페이지 분리
    
    * [feat#34] 카테고리, 학부, 전공 관리 페이지 구현
    
    * fix[#34] API 연동 문제 해결
    
    * feat[#34] 학부, 전공, 카테고리 필드 추가
    
    * [fix#34] 장학금 신청자 페이지 수정
    
    * fix: 장학금 수혜자 페이지
    
    * fix[#34] Scholarship API 하드코딩 수정
    
    * build file upload test
    
    * hotfix: 중복 call 수정
    
    * feat[#27] Student, Manager API 정리
    
    * feat[#27] 장학금 API 수정
    
    * hotfix: 학생 수정 API 문제 해결
    
    * [feat#27] 장학금 수혜자 차트 구현
    
    * [feat#27] 학부, 전공, 카테고리 CRUD API 구현
    
    * [fix#27] 전공 API 에러 수정
    
    * [fix#27] API 전체 경로 수정
    
    * [fix#27] 관리자 권한 API 경로 수정
    
    * [fix#27] 학부, 카테고리 CRUD API 수정
    
    * hotfix: student page loading error fix
    
    * hotfix: deleted unused tab
    
    * [feat#27] 마일리지 수혜자 차트 더미데이터 추가
    
    * [fix#27] 테이블 사이즈 및 위치 수정
    
    * [fix#27] 공지 컨테이너 페이퍼 적용
    
    * [feat#27]마일리지 수혜자  차트 API 연결
    
    * hotfix: router 충돌 해결
    
    * hotfix: 마일리지 차트 데이터 수정
    
    * hotfix: 마일리지 차트 데이터 수정
    
    * [fix#27] 마일리지 차트 API 연동 완료
    
    * hotfix: set default semester
    
    * [fix#27] 로그 기록 삭제
    
    * fix: 학생 이미지 하드코딩 삭제
    
    * [feat#106] 학부 카드 이미지 추가
    
    * [feat#106] 전공 이미지 추가
    
    * [feat#106] 학부, 전공 불필요 칼럼 제거

--- src/components/Department/DeptPage.js
+++ src/components/Department/DeptPage.js
@@ -1,42 +1,47 @@
-import { DataGrid, GridActionsCellItem, GridToolbar } from '@mui/x-data-grid';
-import CustomNoRowsOverlay from '../Department/CustomNoRowsOverlay';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutline';
+import { DataGrid, GridActionsCellItem, GridToolbar } from "@mui/x-data-grid";
+import CustomNoRowsOverlay from "../Department/CustomNoRowsOverlay";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutline";
 
 const columns = [
   {
-    field: 'id',
-    headerName: '번호',
+    field: "id",
+    headerName: "번호",
     width: 80,
   },
   {
-    field: 'name',
-    headerName: '학부',
-    width: 200,
+    field: "name",
+    headerName: "학부",
+    width: 900,
   },
-  {
-    field: 'professor',
-    headerName: '지도교수',
-    width: 150,
-  },
-  {
-    field: 'phone',
-    headerName: '사무실 번호',
-    width: 250,
-  },
-  {
-    field: 'location',
-    headerName: '학부 위치',
-    width: 150,
-  },
-  {
-    field: 'total',
-    headerName: '총 인원',
-    width: 150,
-  },
+  // {
+  //   field: 'professor',
+  //   headerName: '지도교수',
+  //   width: 150,
+  // },
+  // {
+  //   field: 'phone',
+  //   headerName: '사무실 번호',
+  //   width: 250,
+  // },
+  // {
+  //   field: 'location',
+  //   headerName: '학부 위치',
+  //   width: 150,
+  // },
+  // {
+  //   field: 'total',
+  //   headerName: '총 인원',
+  //   width: 150,
+  // },
 ];
 
-function DeptPage({ departments, setCurrentId, handleOpenEdit, handleDeleteClick }) {
+function DeptPage({
+  departments,
+  setCurrentId,
+  handleOpenEdit,
+  handleDeleteClick,
+}) {
   return (
     <DataGrid
       components={{
@@ -54,11 +59,11 @@ function DeptPage({ departments, setCurrentId, handleOpenEdit, handleDeleteClick
       columns={[
         ...columns,
         {
-          field: 'actions',
-          type: 'actions',
-          headerName: '기능',
+          field: "actions",
+          type: "actions",
+          headerName: "기능",
           width: 128,
-          cellClassName: 'actions',
+          cellClassName: "actions",
           getActions: ({ departmentId }) => {
             return [
               <GridActionsCellItem
